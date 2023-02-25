e85f0e77e855074da175911de14f8907797f2117
Oleh <choleg22@gmail.com>
2023-02-25 10:43:24 +0200

    add

---
+++ index3.js
@@ -0,0 +1,59 @@
+// const courses = [
+//   {
+//     name: "Basic HTML+CSS",
+//     topics: ["VSCode", "HTML", "CSS", "GitHub", "GitHub Desctop"],
+//   },
+//   {
+//     name: "Intermediate HTML+CSS",
+//     topics: ["VSCode", "HTML", "CSS", "GitHub", "Git", "Terminal"],
+//   },
+//   {
+//     name: "Basic JavaScript",
+//     topics: [
+//       "VSCode",
+//       "Type system",
+//       "Loops",
+//       "Function",
+//       "Git",
+//       "Conditions",
+//       "Classes",
+//       "GitHub",
+//       "DOM",
+//     ],
+//   },
+//   {
+//     name: "Intermediate JavaScript",
+//     topics: [
+//       "VSCode",
+//       "NPM",
+//       "Bundlers",
+//       "Transpiling",
+//       "Git",
+//       "Promises",
+//       "AJAX",
+//       "GitHub",
+//     ],
+//   },
+// ];
+
+
+//Зібрати в allTopics масив всіх предметів всіх курсів
+//Виконати фільтрацію, залишивши в uniqueTopics тільки унікальні елементи
+
+
+// const allTopics = courses.flatMap(course => course.topics).filter((course, index, array) => array.indexOf(course) === index);
+// console.log(allTopics);
+
+
+//TODO:==============================================
+//Призначити знижку 20% на фрукти в масиві,
+//Присвоїти ID для кожного продукту
+
+// const fruits = [
+//   { name: "apple", price: 200 },
+//   { name: "orange", price: 300 },
+//   { name: "grapes", price: 750 },
+// ];
+
+// const discount = fruits.map(fruit => { return {...fruit, price: fruit.price * 0.8, id: Date.now()}});
+// console.log(discount);
