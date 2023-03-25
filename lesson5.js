6f168ee8893ce77c3ca9c74e1a226b29d1e4043c
YuliaMoore <115689321+YuliaMoore@users.noreply.github.com>
2023-03-25 13:12:25 +0200

    last 5 lesson

--- lesson5.js
+++ lesson5.js
@@ -54,3 +54,152 @@
 //     btn.textContent = "Скрыть";
 //   }
 // }
+/*
+Завдання 4
+Кнопка "Зменшити" робить квадрат менше на 10 пікселів, допопка "Збільшити" - більше на 10 пікселів.
+*/
+// const box = document.querySelector("#box");
+// const decreaseBtn = document.querySelector("#decrease");
+// const increaseBtn = document.querySelector("#increase");
+
+// decreaseBtn.addEventListener("click", () => {
+//   let boxStyle = window.getComputedStyle(box).width;
+//   const parthBox = parseInt(boxStyle);
+//   box.style.width = parthBox - 10 + "px";
+//   box.style.height = parthBox - 10 + "px";
+
+//   console.log(parthBox);
+// });
+
+// increaseBtn.addEventListener("click", () => {
+//   let boxStyle = window.getComputedStyle(box).width;
+//   const parthBox = parseInt(boxStyle);
+//   box.style.width = parthBox + 10 + "px";
+//   box.style.height = parthBox + 10 + "px";
+
+//   console.log(parthBox);
+// });
+//TODO:======================
+// Завдання 5
+//  Створіть HTML сторінку з табличкою,
+//  яка містить список продуктів.Кожен рядок у
+//  табличці повинен містити назву продукту та його ціну.
+//  При натисканні на будь - який рядок у табличці відобразіть
+//  повідомлення з назвою продукту та його ціною.
+
+// const productTable = document.querySelector("#productTable");
+// const details = document.querySelector("#productDetails");
+// productTable.addEventListener("click", (event) => {
+//   const productTarget = event.target.parentNode;
+
+//   const productName = productTarget.cells[0].textContent;
+//   const productPrice = productTarget.cells[1].textContent;
+//   details.textContent = `name:${productName}, price:${productPrice}`;
+// });
+//TODO:======================
+// Завдання 6
+// Створіть HTML сторінку з формою,
+// яка містить поле введення для введення
+// імені користувача та кнопку.При натисканні
+// на кнопку відобразіть повідомлення з привітанням з іменем користувача.
+
+// const username = document.querySelector("#username");
+// const button = document.querySelector("#greetingButton");
+// const message = document.querySelector("#greetingMessage");
+
+// button.addEventListener("click", () => {
+//   message.textContent = `Привіт ${username.value}`;
+// });
+//TODO:======================
+// Завдання 7
+// Написати функцію, яка буде створювати список подій клавіатури
+// event.key та event.code
+// Додати класи на список eventList, на елементи eventCode та eventKey
+// const thumb = document.querySelector(".eventThumb");
+
+// document.addEventListener("keydown", (event) => {
+//   const markUp = `<ul class="eventList">
+//     <li class="eventCode"> ${event.code}</li>
+//     <li class="eventKey"> ${event.key}</li>
+//     </ul>`;
+
+//   thumb.insertAdjacentHTML("beforeend", markUp);
+// });
+//TODO:======================
+// Завдання 8
+
+// Взяти попереднє завдання з Зоопарком та вивести звірів, їх тип та особливості в DOM.
+// Додати до списку клас animalList.На елемент списку animalElement
+class Animal {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
+
+  getInfo() {
+    return `name: ${this.name}, type: ${this.type}`;
+  }
+
+  feed() {
+    console.log(`feeding ${this.name} travoi`);
+  }
+}
+
+class Bird extends Animal {
+  constructor(name, type, wingspan) {
+    super(name, type);
+    this.wingspan = wingspan;
+  }
+
+  getInfo() {
+    return `${super.getInfo()}, wingspan: ${this.wingspan}`;
+  }
+
+  feed() {
+    super.feed();
+  }
+}
+
+// grus.feed();
+
+class Hunter extends Animal {
+  constructor(name, type, mane) {
+    super(name, type);
+    this.mane = mane;
+  }
+
+  getInfo() {
+    return `${super.getInfo()}, mane: ${this.mane}`;
+  }
+
+  feed() {
+    super.feed();
+  }
+}
+
+// lion.getInfo();
+
+class Zoo {
+  constructor() {
+    this.animals = [];
+  }
+  addAnimals(someAnimal) {
+    return this.animals.push(someAnimal);
+  }
+
+  getAllAnimals() {
+    this.animals.forEach((animal) => {
+      console.log(animal.getInfo());
+    });
+  }
+}
+
+const zoo = new Zoo();
+
+const lion = new Hunter("Лев", "Охотник", "Пишна");
+zoo.addAnimals(lion);
+
+const grus = new Bird("Журавель", "Сельская", 2);
+zoo.addAnimals(grus);
+
+zoo.getAllAnimals();
