a8c2104387abcd957dddd87c06ea0044240b4232
EV7EV <ev@kneu.edu.ua>
2023-03-25 11:36:40 +0200

    add js5/lesson

---
+++ lesson5.js
@@ -0,0 +1,56 @@
+//TODO:==============================================
+/*
+Завдання 1
+Натискання на кнопку "SHOW ME" має виводити значення з поля введення (дивіться на елементи в html-розмітці)
+*/
+// const btn = document.querySelector("#alertButton");
+// const input = document.querySelector("#alertInput");
+
+// btn.addEventListener("click", onBtnClick);
+
+// function onBtnClick() {
+//   console.log(input.value);
+// }
+
+//TODO:==============================================
+/*
+Завдання 2
+Після натискання кнопки "SWAP ME" здійснюється обмін вмістом між двома інпутами.
+Ви можете натиснути на неї кілька разів або вручну змінити вміст інпутів.
+*/
+
+// const btn = document.querySelector("#swapButton");
+// const input = document.querySelector("#leftSwapInput");
+// const secondInput = document.querySelector("#rightSwapInput");
+
+// btn.addEventListener("click", changeValue);
+
+// function changeValue() {
+//   const valueInput = input.value;
+//   const secondValueInput = secondInput.value;
+//   input.value = secondValueInput;
+//   secondInput.value = valueInput;
+// }
+
+//TODO:==============================================
+/*
+Завдання 3
+Кнопка "Приховати" ховає текст і замінює назву кнопки на
+"Розкрити", при повторному натисканні текст знову стає доступним
+і кнопка набуває початкового вигляду.
+*/
+
+// const btn = document.querySelector("#passwordButton");
+// const input = document.querySelector("#passwordInput");
+
+// btn.addEventListener("click", onBtnclick);
+
+// function onBtnclick() {
+//   if (input.type === "password") {
+//     input.type = "text";
+//     btn.textContent = "Розкрити";
+//   } else {
+//     input.type = "password";
+//     btn.textContent = "Скрыть";
+//   }
+// }
